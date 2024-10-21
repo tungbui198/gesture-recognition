037d263149140b5be62bc5a5343034fd633efb09
tungbui198 <tungbui198.hust@gmail.com>
2024-10-21 14:53:35 +0700

    feat: add weight initializer

--- src/registry.py
+++ src/registry.py
@@ -3,13 +3,17 @@ from mmengine.registry import MODELS as MMENGINE_MODELS
 from mmengine.registry import DATASETS as MMENGINE_DATASETS
 from mmengine.registry import METRICS as MMENGINE_METRICS
 from mmengine.registry import TRANSFORMS as MMENGINE_TRANSFORMS
+from mmengine.registry import WEIGHT_INITIALIZERS as MMENGINE_WEIGHT_INITIALIZERS
 
 
-MODELS = Registry(
-    'model', parent=MMENGINE_MODELS, locations=['src.models'])
-DATASETS = Registry(
-    'dataset', parent=MMENGINE_DATASETS, locations=['src.datasets'])
-METRICS = Registry(
-    'metric', parent=MMENGINE_METRICS, locations=['src.utils'])
+MODELS = Registry("model", parent=MMENGINE_MODELS, locations=["src.models"])
+DATASETS = Registry("dataset", parent=MMENGINE_DATASETS, locations=["src.datasets"])
+METRICS = Registry("metric", parent=MMENGINE_METRICS, locations=["src.utils"])
 TRANSFORMS = Registry(
-    'transform', parent=MMENGINE_TRANSFORMS, locations=['src.datasets.transforms'])
+    "transform", parent=MMENGINE_TRANSFORMS, locations=["src.datasets.transforms"]
+)
+WEIGHT_INITIALIZERS = Registry(
+    "weight initializer",
+    parent=MMENGINE_WEIGHT_INITIALIZERS,
+    locations=["src.utils"],
+)
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,3 @@
 from .typing import *
 from .metrics import *
+from .weight_init import *
